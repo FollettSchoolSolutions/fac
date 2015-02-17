e85eb8a26dbf706f06f7b5f9aaeb4a13b19e07c8
Joe Zirilli <jzirilli@follett.com>
2015-02-17 16:46:15 -0500

    removed .tmp

--- .tmp/templates.js
+++
@@ -1,8 +0,0 @@
-angular.module('fac').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('directives/dateshim/dateshim.html',
-    "<div data-ng-show=\"pickerSupported\"> <input name=\"dateShimNativePicker\" type=\"date\" placeholder=\"mm/dd/yyyy\" data-ng-model=\"clean.text\"> </div> <div data-ng-show=\"!pickerSupported\"> <input name=\"dateShimTextOnly\" type=\"text\" placeholder=\"mm/dd/yyyy\" data-ng-model=\"unclean.text\"> </div>"
-  );
-
-}]);
